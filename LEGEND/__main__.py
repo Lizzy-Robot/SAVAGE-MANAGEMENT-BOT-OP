dc1fcf537c5b32303f80ad14db25b2c2acaba5c0
Lizzy-Robot <81688952+Lizzy-Robot@users.noreply.github.com>
2021-06-24 09:18:44 +0530

    Update __main__.py

--- LEGEND/__main__.py
+++ LEGEND/__main__.py
@@ -225,7 +225,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "I'm Already Online!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            "I'm Already Online!!\n<b>Haven't slept since:</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
